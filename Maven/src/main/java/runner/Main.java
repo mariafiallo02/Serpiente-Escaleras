4406276acf0b25cd59c56807a84f7a51bc3e40cf
SilviaEspinosa <silvia.espinosa@uptc.edu.co>
2026-09-07 22:32:58 -0500

    hola

--- Maven/src/main/java/runner/Main.java
+++ Maven/src/main/java/runner/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Calculadora");
+        System.out.println("holaaaaaa");
         System.out.print("Primer numero: ");
         double firstNumber = scanner.nextDouble();
 
